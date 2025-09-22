086e77c45104208adc7e9ef83a6f1596bf20146f
tmlvl <90562442+tmlvl@users.noreply.github.com>
2025-09-23 01:01:59 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,9 @@ sudo apt-get update
 echo "Base apps installing"
 sudo apt install -y vim unzip net-tools wget curl git xclip
 
-echo "Clone dotfiles..."
-git clone https://github.com/tmlvl/linux-setup.git ~/linux-setup
+echo "start setup..."
+#git clone https://github.com/tmlvl/linux-setup.git ~/linux-setup
 bash ~/linux-setup/dotfiles/setup.sh
+. ~/.bashrc
 
 echo "complete."
