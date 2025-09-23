d542a288c86811d34d866b8981778f041536c75c
tmlvl <90562442+tmlvl@users.noreply.github.com>
2025-09-23 20:46:59 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,4 +10,8 @@ echo "start setup..."
 #git clone https://github.com/tmlvl/linux-setup.git ~/linux-setup
 bash ~/linux-setup/dotfiles/setup.sh
 
+if [-f ~/.bashrc]; then
+  . ~/.bashrc
+fi
+
 echo "complete."
