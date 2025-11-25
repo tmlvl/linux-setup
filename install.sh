ebc1483b8028a8a972f91d229f75aca82093158d
tmlvl <90562442+tmlvl@users.noreply.github.com>
2025-11-25 13:32:56 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ sudo apt install -y tree openssh-server vim unzip net-tools wget curl git tmux
 
 echo "start setup..."
 #git clone https://github.com/tmlvl/linux-setup.git ~/linux-setup
-bash ~/linux-setup/dotfiles/setup.sh
+bash $(pwd)/linux-setup/dotfiles/setup.sh
 
-if [ -f ~/.bashrc ]; then
-  . ~/.bashrc
+if [ -f $(pwd)/.bashrc ]; then
+  . $(pwd)/.bashrc
 fi
 
 echo "complete."
